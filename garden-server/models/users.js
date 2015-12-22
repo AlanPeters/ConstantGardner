7858a50ae96c9439c669684aae5856fdc80b9d92
jsellers <jsellers@commercialtribe.com>
2015-12-21 18:45:07 -0700

    Add lots of stuff
    Added register and login pages using passport for auth. Added tests for
    user login.  Added a test script. Lots of stuff.

---
+++ garden-server/models/users.js
@@ -0,0 +1,12 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema, 'user');
